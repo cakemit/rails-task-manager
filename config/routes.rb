e5723a99c6d2d91050caf8b91b11d1d57ca527e0
Claudia Tanaka <catanaka@hotmail.com>
2020-11-26 19:01:59 -0300

    task manager done YaY uHu dancinha

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks' => 'tasks#index'
 
+  get '/tasks/new' => 'tasks#new'
+  post '/tasks' => 'tasks#create'
+
   get '/tasks/:id' => 'tasks#show', as: :task
+
+  get '/tasks/:id/edit' => 'tasks#edit', as: :task_edit
+  patch 'tasks/:id' => 'tasks#update'
+
+  delete '/tasks/:id' => 'tasks#destroy'
 end
